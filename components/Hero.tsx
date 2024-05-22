fd1542edcb6e8efe253bff8533331517295e84dd
navneet9971 <Navneetjha2012@gmail.com>
2024-05-22 12:55:57 +0530

    Hero Scrtion is done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,7 @@
+import { FaLocationArrow } from 'react-icons/fa'
+import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -13,7 +16,9 @@ const Hero = () => {
         </div>
 
         <div className="h-screen w-full dark:bg-black-100 bg-white  
-        dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        dark:bg-grid-white/[0.03] bg-grid-black/[0.2] 
+        flex items-center justify-center 
+        absolute top-0 left-0">
       
       <div className="absolute pointer-events-none 
       inset-0 flex items-center justify-center 
@@ -30,6 +35,26 @@ const Hero = () => {
             text-xs text-center text-blue-100 max-w-80'>
                 Dynamic Web Magic with Next.js
             </h2>
+
+            <TextGenerateEffect 
+            className='text-center text-[40px] md:text-5xl
+            lg:text-6xl'
+            words='Transforming Concepts into Seamless Experiences'
+            />
+
+            <p className='text-center md:tracking-wider mb-4 text-sm
+            md:text-lg lg:text-2xl'>
+               Hi, I&apos;m Navneet, a Frontend Developer 
+               based in Delhi
+            </p>
+
+            <a href='#about'>
+                <MagicButton
+                title= "Show my work"
+                icon= {<FaLocationArrow />}
+                position='right'
+                />
+            </a>
         </div>
 
 
