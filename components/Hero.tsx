9b4b7f49ecfff237fdfcd414d25cfd85cd2f6d25
navneet9971 <Navneetjha2012@gmail.com>
2024-07-16 00:28:39 +0530

    Check

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,11 +18,11 @@ const Hero = () => {
     try {
       const link = document.createElement('a');
       link.href = "https://drive.google.com/uc?export=download&id=14TjMwXeawmbIJl0fumhbVYy5jN1KBIjt";
-      link.setAttribute('download', 'Navneet Resume.pdf'); 
+      link.setAttribute('download', 'Navneet Resume.pdf');
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-  
+
       // After a 1-second delay, view the resume
       setTimeout(() => {
         window.open("https://drive.google.com/file/d/14TjMwXeawmbIJl0fumhbVYy5jN1KBIjt/view?usp=sharing", "_blank");
@@ -31,87 +31,87 @@ const Hero = () => {
       console.error(err);
     }
   };
-  
+
 
   return (
     <div className='pb-20 pt-36'>
-     
-        <div>
-       <Spotlight className='-top-40 -left-10 md:-left-32
-       md:-top-20 h-screen' fill='white'/>
-       <Spotlight className='top-10 left-full h-[80vh]
-       w-[50vw]' fill='purple'/>
-       <Spotlight className='top-28 left-80 h-[80vh]
-       w-[50vw]' fill='blue'/>
-        </div>
 
-        <div className="h-screen w-full dark:bg-black-100 bg-white  
+      <div>
+        <Spotlight className='-top-40 -left-10 md:-left-32
+       md:-top-20 h-screen' fill='white' />
+        <Spotlight className='top-10 left-full h-[80vh]
+       w-[50vw]' fill='purple' />
+        <Spotlight className='top-28 left-80 h-[80vh]
+       w-[50vw]' fill='blue' />
+      </div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  
         dark:bg-grid-white/[0.03] bg-grid-black/[0.2] 
         flex items-center justify-center 
         absolute top-0 left-0">
-      
-      <div className="absolute pointer-events-none 
+
+        <div className="absolute pointer-events-none 
       inset-0 flex items-center justify-center 
       dark:bg-black-100 bg-white 
       [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-    </div>
+      </div>
 
 
-    <div className='flex justify-center relative my-20
+      <div className='flex justify-center relative my-20
     z-10'>
         <div className='max-w-[89vw] md:max-w-2xl
         lg:max-w-[60vw] flex flex-col items-center 
         justify-center'>
-            {/* <h2 className='uppercase tracking-widest
+          {/* <h2 className='uppercase tracking-widest
             text-xs text-center text-blue-100 max-w-80'>
                 Dynamic Web Magic with Next.js
             </h2> */}
 
-            <TextGenerateEffect 
+          <TextGenerateEffect
             className='text-center text-[40px] md:text-5xl
             lg:text-6xl'
             words='Transforming Concepts into Seamless User Experiences'
-            />
-<p className="flex items-center justify-center text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-    Hello, I&apos;m 
-     <LinkPreview url='https://x.com/Navi9971'>
-      <span className='ml-2 text-purple font-bold'> Navneet Kumar,</span>
-    </LinkPreview>
-     a Frontend Developer based in 
-     
-     <LinkPreview url={image.delhi}>
-     <span className='ml-2 text-purple font-bold'>Delhi</span>
-     </LinkPreview>
-    </p>
-    {/* <FloatingIcons>
+          />
+          <p className="flex items-center justify-center text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hello, I&apos;m
+            <LinkPreview url='https://x.com/Navi9971'>
+              <span className='ml-2 text-purple font-bold'> Navneet Kumar,</span>
+            </LinkPreview>
+            a Frontend Developer based in
+
+            <LinkPreview url={image.delhi}>
+              <span className='ml-2 text-purple font-bold'>Delhi</span>
+            </LinkPreview>
+          </p>
+          {/* <FloatingIcons>
       <img id="x" src={xlogo.src} width={40} height={40} className="cursor-pointer" alt="Developer logo" />
       <img id="git" src={github.src} width={26} height={26} className="cursor-pointer" alt="Developer logo" />
       <img id="linkdin" src={linkdin.src} width={27} height={27} className="cursor-pointer" alt="Developer logo" />
     </FloatingIcons> */}
-    
 
-<div className='flex items-center justify-center gap-10'>
+
+          <div className='flex items-center justify-center gap-10'>
             <a href='#project'>
-                <MagicButton
-                title= "Show my work"
-                icon= {<FaLocationArrow />}
+              <MagicButton
+                title="Show my work"
+                icon={<FaLocationArrow />}
                 position='right'
-                />
+              />
             </a>
             <a>
-                <MagicButton
+              <MagicButton
                 handleClick={handleResume}
-                title= "Resume"
-                icon= {<FaDownload />}
+                title="Resume"
+                icon={<FaDownload />}
                 position='right'
-                />
+              />
             </a>
-            </div>
+          </div>
         </div>
 
 
+      </div>
     </div>
-        </div>
   )
 }
 
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,99 +10,99 @@ const RecentProjects = () => {
   return (
 
     <div className='py-20' id="projects">
-            <h1 className='heading'>
-                Recent project showcase:{' '}
-                <span className='text-purple'> innovation in action
-                </span>
-            </h1>
-            <div
-                className='flex flex-wrap items-center 
+      <h1 className='heading'>
+        Recent project showcase:{' '}
+        <span className='text-purple'> innovation in action
+        </span>
+      </h1>
+      <div
+        className='flex flex-wrap items-center 
         justify-center p-4 gap-x-24 gap-y-8 mt-10' >
 
-{projects.map(({ id, title, des, img, iconLists, live, github, perview, gitText }) => (
-    <CardContainer className="inter-var">
-        
-      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-        
-          <div key={id}>
-            <CardItem
-              translateZ="50"
-              className="text-xl font-bold text-neutral-600 dark:text-white"
-            >
-              {title}
-            </CardItem>
-            <CardItem
-              as="p"
-              translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-            >
-              {des}
-            </CardItem>
-            <CardItem
-              translateZ="100"
-              rotateX={20}
-              rotateZ={-5}
-              className="w-full mt-4"
-            >
-              <Image
-                src={img}
-                height="1000"
-                width="1000"
-                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                alt="thumbnail"
-              />
-            </CardItem>
+        {projects.map(({ id, title, des, img, iconLists, live, github, perview, gitText }) => (
+          <CardContainer className="inter-var">
+
+            <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+
+              <div key={id}>
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                  {title}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  {des}
+                </CardItem>
+                <CardItem
+                  translateZ="100"
+                  rotateX={20}
+                  rotateZ={-5}
+                  className="w-full mt-4"
+                >
+                  <Image
+                    src={img}
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
 
 
-            <div className="flex items-center justify-between mt-7 mb-3">
-   
-              <CardItem
-                translateZ={20}
-                translateX={-40}
-                className='flex items-center'
-              >
-                {iconLists.map((icon, index) => (
-                  <div
-                    key={index}
-                    className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center'
-                    style={{ transform: `translateX(-${5 * index * 2}px)` }}
+                <div className="flex items-center justify-between mt-7 mb-3">
+
+                  <CardItem
+                    translateZ={20}
+                    translateX={-40}
+                    className='flex items-center'
                   >
-                    <img src={icon} alt={icon} className='p-2' />
+                    {iconLists.map((icon, index) => (
+                      <div
+                        key={index}
+                        className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center'
+                        style={{ transform: `translateX(-${5 * index * 2}px)` }}
+                      >
+                        <img src={icon} alt={icon} className='p-2' />
+                      </div>
+                    ))}
+
+                  </CardItem>
+
+                  <div className='flex flex-row gap-5  justify-center items-center'>
+                    <CardItem
+                      translateZ={20}
+                      translateX={40}
+                    >
+                      <Link href={live} target="_blank" rel="noopener noreferrer" className="hover-red-600">
+                        {perview}
+                      </Link>
+
+                    </CardItem>
+
+                    <CardItem
+                      translateZ={20}
+                      translateX={40}
+                    >
+                      <Link href={github as string} target="_blank" rel="noopener noreferrer" className="hover-red-600">
+                        {gitText}
+                      </Link>
+
+                    </CardItem>
+
                   </div>
-                ))}
-             
-              </CardItem>
-             
-              <div className='flex flex-row gap-5  justify-center items-center'>
-              <CardItem
-                translateZ={20}
-                translateX={40}
-              >
-               <Link href={live} target="_blank" rel="noopener noreferrer" className="hover-red-600">
-  {perview}
-</Link>
-
-              </CardItem>
-
-              <CardItem
-                translateZ={20}
-                translateX={40}
-              >
-                <Link  href={github as string} target="_blank" rel="noopener noreferrer" className="hover-red-600">
-                                        {gitText}
-                                    </Link>
-
-              </CardItem>
-
+                </div>
               </div>
-            </div>
-          </div>
-     
-      </CardBody>
-        
-    </CardContainer>
- ))}
-    </div>
+
+            </CardBody>
+
+          </CardContainer>
+        ))}
+      </div>
     </div>
   );
 }
